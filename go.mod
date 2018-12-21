693e389cd184d9eb5be45d83351ca3aec9d02da2
Martin Sommer <martin.sommer@staffbase.com>
2018-12-21 16:23:09 +0100

    :sparkles: prometheus integration plus module settings

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Staffbase/go-measure-things
+
+require github.com/prometheus/client_golang v0.9.2 // indirect
--- src/main.go
+++ src/main.go
@@ -1,8 +1,18 @@
 package main
 
 import (
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"net/http"
 	"strings"
+)
+
+var (
+	greetings = promauto.NewCounter(prometheus.CounterOpts{
+		Name: "things_send_greetings_total",
+		Help: "How often the greetings where send.",
+	})
 )
 
 func sayHello(w http.ResponseWriter, r *http.Request) {
@@ -10,9 +20,11 @@ func sayHello(w http.ResponseWriter, r *http.Request) {
 	message = strings.TrimPrefix(message, "/")
 	message = "Hello " + message
 	w.Write([]byte(message))
+	greetings.Inc()
 }
 
 func main() {
+	http.Handle("/metrics", promhttp.Handler())
 	http.HandleFunc("/", sayHello)
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		panic(err)
