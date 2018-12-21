bafa15757ff4071477dee41cfce9de0df26cf7df
Martin Sommer <martin.sommer@staffbase.com>
2018-12-21 16:38:53 +0100

    :sparkles: prometheus vector added

--- src/main.go
+++ src/main.go
@@ -8,19 +8,29 @@ import (
 	"strings"
 )
 
-var (
-	greetings = promauto.NewCounter(prometheus.CounterOpts{
-		Name: "things_send_greetings_total",
-		Help: "How often the greetings where send.",
-	})
-)
+// Nice golang examples for prometheus:
+//   https://prometheus.io/docs/guides/go-application/
+var greetings = promauto.NewCounter(prometheus.CounterOpts{
+	Name: "things_sent_greetings_total",
+	Help: "How often the greetings where sent.",
+})
+
+var greetingsName = promauto.NewCounterVec(prometheus.CounterOpts{
+	Name: "things_sent_greetings_personal_total",
+	Help: "How often the greetings where sent to a specific person.",
+}, []string{"name"})
 
 func sayHello(w http.ResponseWriter, r *http.Request) {
 	message := r.URL.Path
 	message = strings.TrimPrefix(message, "/")
 	message = "Hello " + message
-	w.Write([]byte(message))
-	greetings.Inc()
+	_, err := w.Write([]byte(message))
+	if err != nil {
+		greetings.Inc()
+		if message != "" {
+			greetingsName.WithLabelValues(message).Inc()
+		}
+	}
 }
 
 func main() {
