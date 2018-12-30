edce2c28bc4c08d8a05e67a2277916e58485a0aa
Martin Sommer <martin.sommer@staffbase.com>
2018-12-30 18:17:34 +0100

    :sparkles: adding a histogram example

---
+++ src/sleep.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"net/http"
+	"strconv"
+	"time"
+)
+
+var sleeper = promauto.NewHistogram(prometheus.HistogramOpts{
+	Name:    "sleep_",
+	Help:    "The sleeping time the users requested.",
+	Buckets: prometheus.LinearBuckets(.0, 100., 50),
+})
+
+func sleep(w http.ResponseWriter, r *http.Request) {
+	sleepParam := r.URL.Query().Get("sleep")
+	duration, err := strconv.ParseFloat(sleepParam, 32)
+	if err == nil {
+		time.Sleep(time.Duration(duration))
+		sleeper.Observe(duration)
+		_, _ = fmt.Fprintf(w, "Sleept for %f milliseconds.", duration)
+	} else {
+		_, _ = fmt.Fprintf(w, "Could not parse input '%s'", sleepParam)
+	}
+}
+
+func main() {
+	http.Handle("/metrics", promhttp.Handler())
+	http.HandleFunc("/", sleep)
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		panic(err)
+	}
+}
